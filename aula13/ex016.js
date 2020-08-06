eb22ceac9c202cfb1259fb6836f58111a2c6da60
Matheus Rodrigues dos Santos <matheus.rodrisantos@bol.com.br>
2020-08-06 10:22:24 -0300

    Estrutura de repetiçao

---
+++ aula13/ex016.js
@@ -0,0 +1,12 @@
+//repetição javascript 
+var c=1
+while(c<=10){
+    console.log(`Tudo jóia ? `)
+    console.log(`${c}`)
+    c++;
+}
+do{
+    console.log(`Tudo jóia ? `)
+    console.log(`${c}`)
+    c++;
+}while(c<=6)
